6b8d2e76ac2eec8c4aca740e69cf7e7b0ae6862d
Roy <rjperlman@gmail.com>
2024-11-07 08:42:54 -0800

    Upload and edit new javascript code and files.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,14 @@ function anotherHello(hi)
 }
 
 console.log(anotherHello("Hello Everone In Class."));
+
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => 
+    {
+        submitButton.disabled = true
+        e.preventDefault()
+        window.location.href = window.location.origin + '/success.html'
+    })
