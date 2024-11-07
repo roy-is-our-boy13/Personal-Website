8309a26c52ab4062248c726ba3bafa2435ae027d
Roy <rjperlman@gmail.com>
2024-11-07 06:56:18 -0800

    Add some new files.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,2 @@
 
-const helloWorld = "Hello World!";
-
-console.log(helloWorld);
-
-function anotherHello(hi)
-{
-
-    console.log(hi);
-}
-
-console.log(anotherHello("Hello Everone In Class."));
+const helloWorld = "Hello W
