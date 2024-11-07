43421c7104ad5594be975293bbc539585be3fb99
Roy <rjperlman@gmail.com>
2024-11-07 13:30:52 -0800

    Lots of CSS and HTML updates on size and fonts.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,2 @@
 
-const helloWorld = "Hello World!";
-
-console.log(helloWorld);
-
-function anotherHello(hi)
-{
-
-    console.log(hi);
-}
-
-console.log(anotherHello("Hello Everone In Class."));
-
-
-const form = document.querySelector('#form')
-const submitButton = document.querySelector('#submit')
-
-form.addEventListener('submit', (e) => 
-    {
-        submitButton.disabled = true
-        e.preventDefault()
-        window.location.href = window.location.origin + '/success.html'
-    }) 
+const helloWorld = "Hello W
