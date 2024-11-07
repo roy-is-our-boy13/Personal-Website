a24f1c4ea8a49274cbb41639c1f587a3ba07838c
Roy <rjperlman@gmail.com>
2024-11-07 13:53:09 -0800

    More updates with another html file called skills.html

--- javascript.js
+++ javascript.js
@@ -1,2 +1,23 @@
 
-const helloWorld = "Hello W
+const helloWorld = "Hello World!";
+
+console.log(helloWorld);
+
+function anotherHello(hi)
+{
+
+    console.log(hi);
+}
+
+console.log(anotherHello("Hello Everone In Class."));
+
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => 
+    {
+        submitButton.disabled = true
+        e.preventDefault()
+        window.location.href = window.location.origin + '/success.html'
+    }) 
